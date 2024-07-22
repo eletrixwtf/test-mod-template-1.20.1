53292dbfda3bd100cbb643334965d9b58ff5f490
eletrixwtf <165272067+eletrixwtf@users.noreply.github.com>
2024-07-22 19:12:14 +1000

    Adds Tooltips to items

--- src/main/java/net/eletrixgg/testmod/block/custom/SoundBlock.java
+++ src/main/java/net/eletrixgg/testmod/block/custom/SoundBlock.java
@@ -2,14 +2,21 @@ package net.eletrixgg.testmod.block.custom;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class SoundBlock extends Block {
     public SoundBlock(Settings settings) {
@@ -21,5 +28,11 @@ public class SoundBlock extends Block {
                               PlayerEntity player, Hand hand, BlockHitResult hit) {
         world.playSound(player, pos, SoundEvents.BLOCK_NOTE_BLOCK_XYLOPHONE.value(), SoundCategory.BLOCKS,1f, 1f);
         return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable BlockView world, List<Text> tooltip, TooltipContext options) {
+        tooltip.add(Text.literal("Makes a sound when right-clicked.")); //The Text (This is literal text meaning that it dosen't need to be in the en_us.json and wont be translated or nothing
+        super.appendTooltip(stack, world, tooltip, options);
     }
 }
